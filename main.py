d01433583c10387b8102e682a8f5dd20f1bfa7bb
Jhonatan Javier Corella Pérez <74806674+JhonCorellaDev@users.noreply.github.com>
2022-12-09 01:05:58 -0300

    Simple Python Code

---
+++ main.py
@@ -0,0 +1,8 @@
+#definomos la funcion calcular
+def calcular(x, y):
+    resultado = x + y
+    print("El resultado de la operación es:" + str(resultado))#realizamos el print de la variable resultado
+
+
+#Aqui llamamos a la función calcular
+calcular(5, 5)
